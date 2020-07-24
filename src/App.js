08ea61be10fe689fd3a1b0b34c17b1ed58b7fa38
bbjorn <bbjorn@student.chalmers.se>
2020-07-24 14:19:34 +0200

    Implemented the Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React from "react";
 import "./App.css";
 import PortHeader from "./components/PortHeader";
+import PortFooter from "./components/PortFooter";
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <PortHeader />
       <div className="App">
         <h1>My react portfolio</h1>
         <h2>Author: Björn Bergqvist </h2>
         <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
+        <p> This is a test line </p>
       </div>
-    </div>
+      <PortFooter
+        linkedIn="bjrn-bergquist"
+        email="bjrn.bergqvist@gmail.com"
+        phonenumber="(+46) 073-3809927"
+      />
+    </React.Fragment>
   );
 }
 
 export default App;
+
+/*
+header
+navbar
+switch -> welcome / about me / design / code
+footer
+*/
---
+++ src/components/PortFooter.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "../css/footer.css";
+import linkedInIcon from "../img/contacticons/LI-In-Bug.png";
+import emailIcon from "../img/contacticons/email.png";
+import phoneIcon from "../img/contacticons/phone.png";
+
+function PortFooter(props) {
+  const { email, linkedIn, phonenumber } = props;
+  return (
+    <div className="footer">
+      <h2 className="footer-h2"> Contact </h2>
+      <ul className="footer-inside">
+        <li>
+          <a
+            className="footer-link"
+            href={"https://www.linkedin.com/in/" + linkedIn + "/"}
+          >
+            <img
+              className="footer-icon"
+              src={linkedInIcon}
+              alt="LinkedIn Icon"
+            />
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a className="footer-link" href={"mailto:" + email}>
+            <img className="footer-icon" src={emailIcon} alt="Email Icon" />{" "}
+            Email
+          </a>
+        </li>
+        <li className="footer-text">
+          <img className="footer-icon" src={phoneIcon} alt="Phone Icon" />
+          {phonenumber}
+        </li>
+        <a className="footer-copyright-link" href="http://www.freepik.com">
+          Icons Designed by Freepik
+        </a>
+      </ul>
+    </div>
+  );
+}
+
+export default PortFooter;
