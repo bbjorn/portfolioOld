7190a4822b807ea32f63b0377f3e7b8d589be4aa
bbjorn <bbjorn@student.chalmers.se>
2020-07-29 11:47:40 +0200

    Footer is now always at the bottom of the page.

--- src/App.js
+++ src/App.js
@@ -9,27 +9,32 @@ import Design from "./components/Design";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/NotFound";
 import "./css/textstyles.css";
+import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
-      <Header
-        position="Interaction Designer and Developer"
-        name="Björn Bergqvist"
-      />
-      <Navbar />
+      <div className="container">
+        <div className="main">
+          <Header
+            position="Interaction Designer and Developer"
+            name="Björn Bergqvist"
+          />
+          <Navbar />
 
-      <main className="container">
-        <Switch>
-          <Route path="/portfolio/design" component={Design} />
-          <Route path="/portfolio/code" component={Code} />
-          <Route path="/portfolio/aboutme" component={AboutMe} />
-          <Route path="/portfolio/start" component={FrontPage} />
-          <Route path="/portfolio/notfound" component={NotFound} />
-          <Redirect from="/portfolio/" exact to="/portfolio/start" />
-          <Redirect to="/portfolio/notfound" />
-        </Switch>
-      </main>
+          <main>
+            <Switch>
+              <Route path="/portfolio/design" component={Design} />
+              <Route path="/portfolio/code" component={Code} />
+              <Route path="/portfolio/aboutme" component={AboutMe} />
+              <Route path="/portfolio/start" component={FrontPage} />
+              <Route path="/portfolio/notfound" component={NotFound} />
+              <Redirect from="/portfolio/" exact to="/portfolio/start" />
+              <Redirect to="/portfolio/notfound" />
+            </Switch>
+          </main>
+        </div>
+      </div>
       <Footer
         linkedIn="bjrn-bergquist"
         email="bjrn.bergqvist@gmail.com"
